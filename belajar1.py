e0d6b5e37c713cf4259d5ea13730fade6ef52bee
Alainz112 <91440622+Alainz112@users.noreply.github.com>
2021-09-27 07:33:56 +0700

    fike awal python pertama

---
+++ belajar1.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     26/09/2021
+# Copyright:   (c) Administrator 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print("show this")
+
+a = 30
+b = 5
+print("nilai a adalah ", a)
+
+print("------INTRODUCING======")
+
+nama = "ripaldi"
+jurusan = "teknik informatika"
+ttl = "31 januari 2002"
+
+print("let me introduce my self")
+print("ny name is  ", nama)
+print("i was born on  ", ttl)
+
+print("=========BATAS=========")
+
+data1 = int(input("masukan angka pertama :") )
+
+data2 = int(input("masukan angka kedua :") )
+
+print("hasil =", data1+data2)
