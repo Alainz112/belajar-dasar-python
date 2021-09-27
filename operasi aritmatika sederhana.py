39fd8aa709e49ee4123be20efb691a76761628cf
Alainz112 <91440622+Alainz112@users.noreply.github.com>
2021-09-27 07:56:41 +0700

    membuat operasi aritmatika sederhana

---
+++ operasi aritmatika sederhana.py
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     27/09/2021
+# Copyright:   (c) Administrator 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print("opeerasi perhitungan sederhana")
+
+#operasi perkalian
+angka1 = int(input("masukan angka pertama :"))
+angka2 = int(input("masukan angka kedua :"))
+hasil = angka1*angka2
+print ("hasil kali =", hasil )
+
+#pangkat
+hasil = angka1**angka2
+print("hasil pangkat =", hasil)
+
+#bagi
+hasil = angka1/angka2
+print("hasil bagi =", hasil)
+
+#penjumlahan
+hasil = angka1+angka2
+print("hasil jumlah =", hasil)
+
+#pengurangan
+hasil = angka1-angka2
+print("hasil kurang =", hasil)
+
+#pangkat
+hasil = angka1%angka2
+print("hasil modulus =", hasil)
